13219099c5ff7791fce1e1724a2af63c2ddbdb5f
Clayton Wakefield <acidduckling@gmail.com>
2018-04-23 09:39:13 +1000

    76 - Bootstrap form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
