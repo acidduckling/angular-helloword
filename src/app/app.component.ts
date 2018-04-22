7a188bf8410f7a3b57f3f4186ce3589f38836b0d
Clayton Wakefield <acidduckling@gmail.com>
2018-04-23 09:25:49 +1000

    Reset solution for section 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FavouriteChangedEventArgs } from './favourite/favourite.component';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +6,5 @@ import { FavouriteChangedEventArgs } from './favourite/favourite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tweet = {
-    body: 'Here is the body of a tweet...',
-    isLiked: false,
-    likesCount: 0
-  }
 
-  post = {
-    title: 'Title',
-    isFavourite: true
-  }
-
-  onFavouriteChange(eventArgs: FavouriteChangedEventArgs) {
-    console.log('favourite changed', eventArgs);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { AuthorsService } from './authors.service';
-import { AuthorsComponent } from './authors/authors.component';
-import { CoursesComponent } from './courses.component';
-import { CoursesService } from './courses.service';
-import { SummaryPipe } from './summary.pipe';
-import { FavouriteComponent } from './favourite/favourite.component';
-import { TitleCasePipe } from './title-case.pipe';
-import { PanelComponent } from './panel/panel.component';
-import { LikeComponent } from './like/like.component';
-
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
-    AuthorsComponent,
-    SummaryPipe,
-    FavouriteComponent,
-    TitleCasePipe,
-    PanelComponent,
-    LikeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [
-    CoursesService,
-    AuthorsService
+
   ],
   bootstrap: [AppComponent]
 })
