cf74f9243f0925465cfd5a4202dc3382dffa8cd3
Clayton Wakefield <acidduckling@gmail.com>
2018-04-23 19:49:47 +1000

    Assignment 6 - Course Form - My Solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactFormComponent,
+    CourseFormComponent,
   ],
   imports: [
     BrowserModule,
