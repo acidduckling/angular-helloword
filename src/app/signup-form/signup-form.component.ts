307dbaff1343080530de976d54b29afa81e3ba33
Clayton Wakefield <acidduckling@gmail.com>
2018-04-24 15:59:22 +1000

    100 - Nested Form Groups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -3,20 +3,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsernameValidators } from './username.validators';
 
 @Component({
-  selector: 'signup-form',
-  templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.css']
+  selector: "signup-form",
+  templateUrl: "./signup-form.component.html",
+  styleUrls: ["./signup-form.component.css"]
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    'username': new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace
-    ], [
-      UsernameValidators.shouldBeUnique
-    ]),
-    'password': new FormControl('', Validators.required),
+    account: new FormGroup({
+      username: new FormControl(
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          UsernameValidators.cannotContainSpace
+        ],
+        [UsernameValidators.shouldBeUnique]
+      ),
+      password: new FormControl("", Validators.required)
+    })
   });
 
   login() {
@@ -26,9 +30,9 @@ export class SignupFormComponent {
   }
 
   get username() {
-    return this.form.get('username');
+    return this.form.get("account.username");
   }
   get password() {
-    return this.form.get('password');
+    return this.form.get("account.password");
   }
 }
