18320de85b2c61709fab392bbb016ba6c64e96d3
Clayton Wakefield <acidduckling@gmail.com>
2018-04-24 20:27:20 +1000

    110 - OnInit Interface

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,42 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
-  selector: 'posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  selector: "posts",
+  templateUrl: "./posts.component.html",
+  styleUrls: ["./posts.component.css"]
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts: any[];
-  private url = 'http://jsonplaceholder.typicode.com/posts';
+  private url = "http://jsonplaceholder.typicode.com/posts";
 
-  constructor(private http: Http) {
-    http.get(this.url)
-      .subscribe(response => {
-        this.posts = response.json();
-      });
+  constructor(private http: Http) {}
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe(response => {
+      this.posts = response.json();
+    });
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
-    input.value = '';
+    input.value = "";
 
-    this.http.post(this.url, JSON.stringify(post))
-      .subscribe(response => {
-        post['id'] = response.json().id;
-        this.posts.splice(0, 0, post);
-      });
+    this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
+      post["id"] = response.json().id;
+      this.posts.splice(0, 0, post);
+    });
   }
 
   updatePost(post) {
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true })).subscribe(response => {
-      console.log(response.json());
-    })
+    this.http
+      .patch(this.url + "/" + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response.json());
+      });
     //this.http.put(this.url, JSON.stringify(post));
   }
 
   deletePost(post) {
-    this.http.delete(this.url + '/' + post.id).subscribe(response => {
+    this.http.delete(this.url + "/" + post.id).subscribe(response => {
       let index = this.posts.indexOf(post);
       this.posts.splice(index, 1);
     });
