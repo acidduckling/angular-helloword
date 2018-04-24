3592729e3a40e8c9609ebc84795925476271d994
Clayton Wakefield <acidduckling@gmail.com>
2018-04-24 19:59:48 +1000

    106 - Getting Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { HttpModule, Http } from '@angular/http'
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [
   ],
