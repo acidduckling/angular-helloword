e1837afeb6fea2f3783f2037ab127e39588499ab
Clayton Wakefield <acidduckling@gmail.com>
2018-04-24 15:21:40 +1000

    97 - Asynchronous Validators (using Promise)

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -7,4 +7,16 @@ export class UsernameValidators {
 
         return null;
     }
+
+    static shouldBeUnique(control: AbstractControl) : Promise<ValidationErrors | null> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('ok');
+                if(control.value === 'mosh')
+                    resolve({ shouldBeUnique: true });
+                else 
+                    resolve(null);
+            }, 2000);
+        });
+    }
 }
