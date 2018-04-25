cb7afe72c561411a754c6abf03d17d0a9c16b372
Clayton Wakefield <acidduckling@gmail.com>
2018-04-25 11:49:12 +1000

    120 - Extracting a reusable Error Handling Method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,42 +1,44 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
-import { Observable } from 'rxjs/Observable';
-import { AppError } from '../common/app-error';
-import { NotFoundError } from '../common/not-found-error';
-import { BadInput } from '../common/bad-input';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import "rxjs/add/operator/catch";
+import "rxjs/add/observable/throw";
+import { Observable } from "rxjs/Observable";
+import { AppError } from "../common/app-error";
+import { NotFoundError } from "../common/not-found-error";
+import { BadInput } from "../common/bad-input";
 
 @Injectable()
 export class PostService {
   private url = "http://jsonplaceholder.typicode.com/posts";
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
   getPosts() {
     return this.http.get(this.url);
   }
 
   createPost(post) {
-    return this.http.post(this.url, JSON.stringify(post)).catch((error: Response) => {
-      if(error.status === 400)
-        return Observable.throw(new BadInput(error.json()))
-      
-      return Observable.throw(new AppError(error.json()));
-    })
+    return this.http
+      .post(this.url, JSON.stringify(post))
+      .catch(this.handleError);
   }
 
   updatePost(post) {
-    return this.http.patch(this.url + "/" + post.id, JSON.stringify({ isRead: true }));
+    return this.http
+      .patch(this.url + "/" + post.id, JSON.stringify({ isRead: true }))
+      .catch(this.handleError);
   }
 
   deletePost(id) {
-    return this.http.delete(this.url + "/" + id).catch((error: Response) => {
-      if (error.status === 404)
-        return Observable.throw(new NotFoundError());
-      
-      return Observable.throw(new AppError(error.json()));
-    })
+    return this.http.delete(this.url + "/" + id).catch(this.handleError);
   }
 
+  private handleError(error: Response) {
+    if (error.status === 400)
+      return Observable.throw(new BadInput(error.json()));
+
+    if (error.status === 404) return Observable.throw(new NotFoundError());
+
+    return Observable.throw(new AppError(error.json()));
+  }
 }
