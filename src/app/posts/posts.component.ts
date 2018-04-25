c6261b3a56eb5b40db06719ca1d776c8e9e803e5
Clayton Wakefield <acidduckling@gmail.com>
2018-04-25 10:34:11 +1000

    115 - Handling expected errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { PostService } from '../services/post.service';
+import { Component, OnInit } from "@angular/core";
+import { PostService } from "../services/post.service";
 
 @Component({
   selector: "posts",
@@ -12,44 +12,61 @@ export class PostsComponent implements OnInit {
   constructor(private service: PostService) {}
 
   ngOnInit(): void {
-    this.service.getPosts().subscribe(response => {
-      this.posts = response.json();
-    }, error => {
-      alert('an unexpected error occurred.');
-      console.log(error);
-    });
+    this.service.getPosts().subscribe(
+      response => {
+        this.posts = response.json();
+      },
+      (error: Response) => {
+        alert("an unexpected error occurred.");
+        console.log(error);
+      }
+    );
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = "";
 
-    this.service.createPost(post).subscribe(response => {
-      post["id"] = response.json().id;
-      this.posts.splice(0, 0, post);
-    }, error => {
-      alert('an unexpected error occurred.');
-      console.log(error);
-    });
+    this.service.createPost(post).subscribe(
+      response => {
+        post["id"] = response.json().id;
+        this.posts.splice(0, 0, post);
+      },
+      (error: Response) => {
+        if (error.status === 400) {
+          // this.form.setErrors(error.json());
+        } else {
+          alert("an unexpected error occurred.");
+          console.log(error);
+        }
+      }
+    );
   }
 
   updatePost(post) {
-    this.service.updatePost(post).subscribe(response => {
-      console.log(response.json());
-    }, error => {
-      alert('an unexpected error occurred.');
-      console.log(error);
-    });
+    this.service.updatePost(post).subscribe(
+      response => {
+        console.log(response.json());
+      },
+      (error: Response) => {
+        alert("an unexpected error occurred.");
+        console.log(error);
+      }
+    );
     //this.http.put(this.url, JSON.stringify(post));
   }
 
   deletePost(post) {
-    this.service.deletePost(post.id).subscribe(response => {
-      let index = this.posts.indexOf(post);
-      this.posts.splice(index, 1);
-    }, error => {
-      alert('an unexpected error occurred.');
-      console.log(error);
-    });
+    this.service.deletePost(post.id).subscribe(
+      response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      },
+      (error: Response) => {
+        if (error.status === 404) alert("This post has already been deleted.");
+        else alert("an unexpected error occurred.");
+        console.log(error);
+      }
+    );
   }
 }
