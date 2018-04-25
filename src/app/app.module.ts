e47da82acb3f3c5895bc4739d483f1e231506e78
Clayton Wakefield <acidduckling@gmail.com>
2018-04-25 10:18:59 +1000

    112 - Extracting a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { PostService } from './services/post.service';
 
 
 
@@ -19,6 +20,7 @@ import { PostsComponent } from './posts/posts.component';
     HttpModule
   ],
   providers: [
+    PostService
   ],
   bootstrap: [AppComponent]
 })
