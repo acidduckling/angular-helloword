93016ad0cd45987be4e2976d83c555fa4b2b54b5
Clayton Wakefield <acidduckling@gmail.com>
2018-04-25 11:19:01 +1000

    118 - Importing Observable Operators and Factory Methods

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 import { Observable } from 'rxjs/Observable';
 import { AppError } from '../common/app-error';
 import { NotFoundError } from '../common/not-found-error';
