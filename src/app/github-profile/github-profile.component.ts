48b0d03c21425e44168d1e642610439f75566836
Clayton Wakefield <acidduckling@gmail.com>
2018-04-26 14:44:32 +1000

    132 - Why Route Params are Observables (and using route.snapshot)

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -11,6 +11,15 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    
+    // If route for page (user id for example) can be updated, then we should subscribe to observable, 
+    // otherwise we cam use a snapshot.
+
+    // Using snapshot
+    // let id = this.route.snapshot.paramMap.get('id');
+    // console.log(id);
+
+    // using subscription to Observable
     this.route.paramMap.subscribe((params) => {
       let id = +params.get('id');
       console.log(id);
