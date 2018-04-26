5329b2d8e9388c6558ce51fe89e1b5e35bf9afa3
Clayton Wakefield <acidduckling@gmail.com>
2018-04-26 18:46:17 +1000

    137 - Programmatic Navigation

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,29 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent implements OnInit {
+export class GithubProfileComponent {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
-    
-    // If route for page (user id for example) can be updated, then we should subscribe to observable, 
-    // otherwise we cam use a snapshot.
-
-    // Using snapshot
-    // let id = this.route.snapshot.paramMap.get('id');
-    // console.log(id);
-
-    // using subscription to Observable
-    this.route.paramMap.subscribe((params) => {
-      let id = +params.get('id');
-      console.log(id);
-    });
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
+    })
   }
 
 }
