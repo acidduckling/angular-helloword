3cb034b0a59de9ed1930d017660e5f14ce0ac08a
Clayton Wakefield <acidduckling@gmail.com>
2018-04-26 14:57:47 +1000

    134 - Query Parameters

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubFollowersService } from '../services/github-followers.service';
-import { Follower } from './Follower';
+import { Component, OnInit } from "@angular/core";
+import { GithubFollowersService } from "../services/github-followers.service";
+import { Follower } from "./Follower";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'github-followers',
-  templateUrl: './github-followers.component.html',
-  styleUrls: ['./github-followers.component.css']
+  selector: "github-followers",
+  templateUrl: "./github-followers.component.html",
+  styleUrls: ["./github-followers.component.css"]
 })
 export class GithubFollowersComponent implements OnInit {
   followers: Follower[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private service: GithubFollowersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.service.getAll().subscribe(followers => this.followers = followers);
-  }
+    this.route.paramMap.subscribe(params => {
 
+    });
+
+    this.route.queryParamMap.subscribe(params => {
+      
+    });
+
+    // Using snapshot...
+    //let id = this.route.snapshot.paramMap.get('id');
+
+    // let page = this.route.snapshot.queryParamMap.get('page')
+    // let order = this.route.snapshot.queryParamMap.get('order')
+
+    this.service.getAll().subscribe(followers => (this.followers = followers));
+  }
 }
